250e290b7cc9e4e12486ca038c6d7349d60fce5a
unknown <getachewbiruk990@gmail.com>
2023-05-17 23:19:13 -0700

    navigatation

--- src/controllers/LoginController.java
+++ src/controllers/LoginController.java
@@ -1,38 +1,54 @@
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import com.jfoenix.controls.*;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.image.ImageView;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class LoginController implements Initializable{
+public class LoginController implements Initializable {
 
-    @FXML 
-    private JFXTextField username;
   
-    @FXML 
-    private JFXButton login;
-    @FXML 
-    private JFXPasswordField password;
 
+    @FXML
+    private JFXButton login;
+    @FXML
+    private JFXButton btn;
+
+    @FXML
+    private JFXButton signUp; // Add signUp button
+    @FXML
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-     username.setStyle("-fx-text-inner-color: #a0a2ab");
-     password.setStyle("-fx-text-inner-color: #a0a2ab");   
-     login.setOnAction(event -> {
-    System.out.print("hellow");
-    });
-       
+     
     }
-    @FXML
-    public void in(ActionEvent e){
-        System.out.print("hellow");
-    }
-    
-}
 
+    @FXML
+    private void navigate(ActionEvent event) throws IOException {
+        Stage primaryStage = (Stage) signUp.getScene().getWindow();
+        Parent root = null;
+        if (event.getSource() == signUp) {
+            root = FXMLLoader.load(getClass().getResource("/FXML/SignUp.fxml")); // Load SignUp.fxml for sign-up page
+        } else if (event.getSource() == btn){
+            root = FXMLLoader.load(getClass().getResource("/FXML/login.fxml")); // Load Login.fxml for login page
+        }
+        Scene scene = new Scene(root, 600, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    @FXML
+    public void in(ActionEvent e) {
+        System.out.println("Hello");
+    }
+}
